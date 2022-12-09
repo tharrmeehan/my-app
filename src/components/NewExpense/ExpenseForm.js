b115f234aa828dfc547b34d7f68676f1d3e37e86
Tharrmeehan Krishnathasan <tharrmeehan@icloud.com>
2022-12-09 13:11:03 +0100

    small changes to form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
   return (
     <form className="">
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-          <label>Title</label>
+          <label>Amount</label>
           <input type="number" min="0.01" step="0.01" />
         </div>
         <div className="new-expense__control">
-          <label>Title</label>
+          <label>Date</label>
           <input type="date" min="2019-01-01" step="2022-12-31" />
         </div>
       </div>

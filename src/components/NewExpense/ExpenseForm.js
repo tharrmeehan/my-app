a8e0a182c5728e458c3ef594b1e6de4f07085b78
Tharrmeehan Krishnathasan <tharrmeehan@hotmail.com>
2022-12-09 13:25:12 +0100

    added new branch + reedited expenseform

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,12 +3,18 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
   return (
     <form className="">
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Title</label>
